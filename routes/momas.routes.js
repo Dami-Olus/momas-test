5f31a0e9d564d6aadc721889267b085627f73634
Damola Olusakin <olusakinadedamola@gmail.com>
2024-03-20 14:38:30 -0400

    added route for profile data

--- routes/momas.routes.js
+++ routes/momas.routes.js
@@ -4,5 +4,6 @@ const momasController = require("../controller/momas.controller");
 
 router.post("/auth", momasController.getAuthToken);
 router.get("/cred", momasController.getCredentials);
+router.get('/profileData', momasController.getInstProfData);
 
 module.exports = router;
