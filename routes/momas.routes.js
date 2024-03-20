33d1ba4a24af3a91da918275b9b0050663df0d51
Damola Olusakin <olusakinadedamola@gmail.com>
2024-03-20 11:09:02 -0400

    add get cred controller

--- routes/momas.routes.js
+++ routes/momas.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const momasController = require("../controller/momas.controller");
 
 router.post("/auth", momasController.getAuthToken);
+router.get("/cred", momasController.getCredentials);
 
 module.exports = router;
